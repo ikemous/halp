9f4cf2a88525fb7548455e2487cf5929709569ed
ike barranco <barranco.ike@outlook.com>
2020-11-21 01:14:04 -0700

    started adding background

---
+++ client/src/components/Background.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./background.css";
+
+function Background() {
+    return (
+        <Container className="background" fluid>
+        </Container>
+    );
+}
+
+export default Background;
