5b203607f46b41f06e59338360b4c1aa4c037f0e
ike barranco <barranco.ike@outlook.com>
2020-11-25 07:26:45 -0700

    started foundation for pagination use

--- client/src/components/TicketPagination.tsx
+++ client/src/components/TicketPagination.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, RootStateOrAny } from "react-redux";
 import { Pagination } from "react-bootstrap"
 
 function TicketPagination() {
+    const { queryResults } = useSelector((state: RootStateOrAny) => state.query);
     const { currentPage, pageCount } = useSelector((state: RootStateOrAny) => state.ticketPagination)
+
+    useEffect(() => {
+        console.log(queryResults.length)
+    }, [queryResults]);
 
     return (
         <Pagination>
             <Pagination.First />
             <Pagination.Prev />
-
+            {
+                queryResults?
+                    <>
+                        <Pagination.Item active>{1}</Pagination.Item>
+                    </>
+                    :
+                    <>
+                    <Pagination.Item active>{1}</Pagination.Item>
+                    </>
+            }   
             {/* <Pagination.Item>{1}</Pagination.Item>
             <Pagination.Ellipsis />
             <Pagination.Item>{10}</Pagination.Item>
