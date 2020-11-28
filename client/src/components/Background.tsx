0abef1eae2eb808d44b95102030f884c41a6aee9
ike barranco <barranco.ike@outlook.com>
2020-11-28 13:13:30 -0700

    more opacity tesets

--- client/src/components/Background.tsx
+++ client/src/components/Background.tsx
@@ -5,11 +5,11 @@ import "./background.css";
 function Background() {
     return (
         <Container className="background" fluid>
-            <div className="dot dotOne"></div>
-            <div className="dot dotTwo"></div>
-            <div className="dot dotThree"></div>
-            <div className="dot dotFour"></div>
-            <div className="dot dotFive"></div>
+            <div className="dot dotOne" style={{ opacity: "60%"}}></div>
+            <div className="dot dotTwo" style={{ opacity: "60%"}}></div>
+            <div className="dot dotThree" style={{ opacity: "60%"}}></div>
+            <div className="dot dotFour" style={{ opacity: "100%"}}></div>
+            <div className="dot dotFive" style={{ opacity: "100%"}}></div>
         </Container>
     );
 }
